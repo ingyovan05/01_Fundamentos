d7516d82dfe15ee4b39b09358b3863b1563f4d7a
ingyovan <ingyovansolano@gmail.com>
2024-05-09 17:00:42 -0500

    Inicial

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -3,6 +3,8 @@
 function saludar(nombre){
     console.log(arguments)
     console.log('Hola '+nombre)
+    return [1,2]
+    console.log('Jamas me ejecuto, ya retorne')
 }
 
 const saludar2 = function (nombre){
@@ -13,7 +15,28 @@ const saludar3 = (nombre) => {
     console.log('Hola fecha '+ nombre)
 }
 
-
-saludar('Yovan',44, true)
+const retornoSaludar = saludar('Yovan',44, true)
 saludar2('Yovan')
 saludar3('Yovan')
+
+console.log(retornoSaludar[0],retornoSaludar[1])
+
+function sumar(a,b){
+    return a+b
+}
+const sumar2 = (a,b)=> {
+    return a+b
+}
+const sumar3 = (a,b)=> a+b
+
+console.log(sumar(5,6))
+console.log(sumar2(7,6))
+console.log(sumar3(7,8))
+
+function getAleatorio() {
+    return Math.random()
+}
+const getAleatorio2 = () => Math.random()
+
+console.log(getAleatorio())
+console.log(getAleatorio2())
