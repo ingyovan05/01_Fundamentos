5a88011961cd60ec82b6babb2be0d094b3477e23
ingyovan <ingyovansolano@gmail.com>
2024-05-09 16:52:13 -0500

    Inicial

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,7 +1,19 @@
 
 
-function saludar(){
-    console.log('Hola Mundo')
+function saludar(nombre){
+    console.log(arguments)
+    console.log('Hola '+nombre)
 }
 
-saludar()
+const saludar2 = function (nombre){
+    console.log('Hola Mundo '+ nombre)
+}
+
+const saludar3 = (nombre) => {
+    console.log('Hola fecha '+ nombre)
+}
+
+
+saludar('Yovan',44, true)
+saludar2('Yovan')
+saludar3('Yovan')
