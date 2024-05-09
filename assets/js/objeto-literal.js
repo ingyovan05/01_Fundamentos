ae73fef4a7c9881919cf6206c669f79dc83d91d1
ingyovan <ingyovansolano@gmail.com>
2024-05-09 16:43:13 -0500

    Inicial

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -18,15 +18,25 @@ let personaje = {
 console.log(personaje)
 console.log('Nombre' , personaje.nombre)
 console.log('Edad' , personaje['edad'])
-
 console.log('Coors', personaje.coords)
 console.log('Coors', personaje.coords.lat)
-
 console.log('Cantidad Trajes', personaje.trajes.length)
-
 console.log('Coors', personaje.trajes[personaje.trajes.length-1])
-
 const x ='vivo'
 console.log('Vivo', personaje[x])
-
 console.log ('Ultima Peli: ' , personaje["ultima pelicula"])
+
+delete personaje.edad
+console.log(personaje)
+
+personaje.casado =true
+Object.freeze(personaje)
+Object.freeze(personaje.direccion)
+personaje.dinero =1000000
+const entriesPares = Object.entries(personaje)
+console.log(entriesPares)
+const propiedades = Object.getOwnPropertyNames(personaje)
+console.log(propiedades)
+
+const valores = Object.values(personaje)
+console.log(valores)
